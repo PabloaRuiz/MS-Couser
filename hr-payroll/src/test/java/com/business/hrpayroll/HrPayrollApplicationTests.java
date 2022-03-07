b75b121a734822cacdfccd69f2f9b7a33afe19b5
Pablo <58537127+PabloaRuiz@users.noreply.github.com>
2022-03-07 11:38:52 -0300

    Criação do projeto hr-payroll

---
+++ hr-payroll/src/test/java/com/business/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
